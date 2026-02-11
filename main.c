5dcaf72afe80704cfb78c1ee7674d0b77d9f58be
Grubbs <aneasyonenow@gmail.com>
2026-02-10 18:42:56 -0700

    Update main.c

--- main.c
+++ main.c
@@ -9,23 +9,6 @@ int main( void )
 	/* Perform any hardware setup necessary. */
 	prvSetupHardware();
 	
-	GPIOA->MODER &= ~(0xf << (4));
-	GPIOA->MODER |= (0xa << (4));
-	
-	GPIOA->AFR[0] |= (0x77 << (8));
-	
-	GPIOA->OSPEEDR |= (0xf << 4);
-	
-	GPIOA->PUPDR &= ~(0xf << 4);
-	GPIOA->PUPDR |= (0x5 << 4);
-	
-	GPIOA->OTYPER &= ~(0x3 << 2);
-	
-	RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
-	
-	RCC->CCIPR &= ~RCC_CCIPR_USART2SEL;
-	RCC->CCIPR |= RCC_CCIPR_USART2SEL_0;
-	
 	//enable interrupts
 	USART2->CR1 |= USART_CR1_RXNEIE;
 	USART2->CR1 &= ~USART_CR1_TXEIE;
